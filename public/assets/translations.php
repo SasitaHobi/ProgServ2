273fbd7ef687964ddc83f416c3a33eebf02b6625
PlumsPie <sarah.furrer@hes-so.ch>
2025-10-28 16:39:51 +0100

    Translations added for Homepage, index, view, attributes

---
+++ public/assets/translations.php
@@ -0,0 +1,90 @@
+<?php
+$text_translations=[
+    'en'=> [
+        'homeTitle'=> 'Homepage | Graille',
+        'homeH1'=> 'Homepage',
+        'homeText'=>'Welcome to the Graille homepage.',
+        'homeButton'=> 'Go to food manager',
+        'indexTitle'=>'Food manager | Graille',
+        'indexH1'=>'Food manager',
+        'indexH2'=>'Food list',
+        'indexButton'=>'Add new food item',
+        'viewTitle'=>'View and edit all food items | Graille',
+        'viewH1'=>'View food item',
+        'viewBack'=>'Back to homepage',
+        'viewText'=>'Use this page to view a food item.',
+        'viewDelete'=>'Delete',
+        'viewEdit'=>'Edit'
+    ],
+    'fr'=> [
+        'homeTitle'=> 'Page d\'accueil | Graille',
+        'homeH1'=> 'Page d\'accueil',
+        'homeText'=>'Bienvenue sur la page d\'accueil de Graille.',
+        'homeButton'=> 'Aller à la gestion des aliments',
+        'indexTitle'=>'Gestion des aliments | Graille',
+        'indexH1'=>'Gestion des aliments',
+        'indexH2'=>'Liste des aliments',
+        'indexButton'=>'Ajouter un nouvel aliment',
+        'viewTitle'=>'Visualise et modifie un aliment | Graille',
+        'viewH1'=>'Visualise un aliment',
+        'viewBack'=>'Retour à l\'accueil',
+        'viewText'=>'Utilise cette page pour visualiser un aliment.',
+        'viewDelete'=>'Supprimer',
+        'viewEdit'=>'Mettre à jour'
+
+    ]
+    ];
+    $error_translations=[
+    'en'=> [
+        'createName'=> 'Food name must be at leat two characters.',
+        'createShop'=> 'Shop name must be at leat two characters.',
+        'createQty'=> 'Quantity is required.',
+        'createUnit'=> 'Unit is required.',
+        'createSpot'=> 'Spot is required.'
+    ],
+    'fr'=> [
+        'createName'=> 'Le nom de l\'aliment doit contenir au moins 2 caractères.',
+        'createShop'=> 'Le nom du magasin doit contenir au moins 2 caractères.',
+        'createQty'=> 'La quantité doit être un nombre positif.',
+        'createUnit'=> 'L\'unité est obligatoire.',
+        'createSpot'=> 'Un emplacement est obligatoire.'
+    ]
+    ];
+
+$att_translations=[
+    'en'=> [
+        'name'=> 'Name',
+        'peremption'=> 'Peremption Date',
+        'shop'=> 'Shop',
+        'qty'=> 'Quantity',
+        'unit'=> 'Unit',
+        'spot'=> 'Spot',
+        'pack'=> 'Pack',
+        'l'=> 'Litre',
+        'g'=> 'Gramm',
+        'kilo'=> 'Kilogramm',
+        'piece'=> 'Piece',
+        'cupboard'=> 'Cupboard',
+        'fridge'=> 'Fridge',
+        'freezer'=> 'Freezer',
+        'cellar'=> 'Cellar'
+    ],
+    'fr'=> [
+        'name'=> 'Nom',
+        'peremption'=> 'Date de péremption',
+        'shop'=> 'Magasin',
+        'qty'=> 'Quantité',
+        'unit'=> 'Unité',
+        'spot'=> 'Emplacement',
+        'pack'=> 'Paquet',
+        'l'=> 'Litre',
+        'g'=> 'Gramme',
+        'kilo'=> 'Kilogramme',
+        'piece'=> 'Pièce',
+        'cupboard'=> 'Armoire',
+        'fridge'=> 'Réfrigérateur',
+        'freezer'=> 'Congélateur',
+        'cellar'=> 'Cave'
+    ]
+]
+?>
